f0eda31a7dd27ff36c243c92a6f6b489b7bcfa85
Bill Gloff <billgloff@zerosphere.com>
2014-04-05 16:45:50 -0500

    Finished import logic

--- data_normalizer/app/controllers/data_importer_controller.rb
+++ data_normalizer/app/controllers/data_importer_controller.rb
@@ -1,9 +1,23 @@
 class DataImporterController < ApplicationController
 
-  def index
-  end
+  def index; end
 
   def import
+  	if file_params[:document].blank?
+  		flash[:error] = "File missing"
+  		redirect_to data_importer_index_path
+  	else
+  		file = params[:document].read
+  		import_service = ImportService.new(file)
+  		import_service.parse
+  		@total_amount_gross = import_service.total_amount_gross
+  	end
+  end
+
+private
+
+  def file_params
+  	params.permit(:document)
   end
 
 end
--- data_normalizer/spec/features/data_importers_spec.rb
+++ data_normalizer/spec/features/data_importers_spec.rb
@@ -9,14 +9,24 @@ describe 'Data Importer Page' do
 
     it 'should have an upload form field' do
       visit data_importer_index_path
-      expect(page).to have_xpath('//form/input[@type="file"]')
+      expect(page).to have_xpath('//input[@type="file"]')
     end
   end
 
   describe 'POST /data_importer' do
-    it 'should have instructions' do
+    it "should give an error if no file is selected" do
       visit data_importer_index_path
-      expect(page).to have_content 'Data Importer'
+      click_button 'Import'
+      expect(page).to have_content 'File missing'
+    end
+
+    it 'should be able to submit a tab delimited file' do
+      visit data_importer_index_path
+      file_path = Rails.root + "spec/fixtures/example_input.tab"
+      attach_file('document', file_path)
+      click_button 'Import'
+      expect(page).to have_content 'Total Amount Gross Revenue'
+      expect(page).to have_content '$30.00'
     end
 
   end
